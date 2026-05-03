5313d0057e008c82b4ae41a8723cbbd465ce857b
Diego Nehab <1635557+diegonehab@users.noreply.github.com>
2026-05-03 21:57:08 +0100

    docs: port Limiting execution and State hashes sections to emulator 0.20
    Update recipes/find-htif-putchar.lua to use the generic read_reg("name")
    API introduced in 0.19, replacing the removed per-accessor methods
    read_iflags_H/Y(), read_htif_tohost(), and read_mcycle().
    
    Enable config_dump_nothing, cycles_limit_exec, max_mcycle_arg, limit_exec,
    and all state_hashes_* blocks. Also add enabled=yes to the inline
    []{replace=...} spans in the surrounding prose.

--- doc/recipes/config/nothing-to-do.lua
+++ doc/recipes/config/nothing-to-do.lua
@@ -1,91 +1,280 @@
 return {
-  processor = {
-    x = {
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
-      0x0, -- default
+  cmio = {
+    rx_buffer = {
+      backing_store = {
+        create = false, -- default
+        data_filename = "", -- default
+        dht_filename = "", -- default
+        dpt_filename = "", -- default
+        shared = false, -- default
+        truncate = false, -- default
+      },
     },
-    iflags = 0x18, -- default
-    ilrsc = 0xffffffffffffffff, -- default
-    marchid = 0xc, -- default
-    mcause = 0x0, -- default
-    mcounteren = 0x0, -- default
-    mcycle = 0x0, -- default
-    medeleg = 0x0, -- default
-    mepc = 0x0, -- default
-    mideleg = 0x0, -- default
-    mie = 0x0, -- default
-    mimpid = 0x1, -- default
-    minstret = 0x0, -- default
-    mip = 0x0, -- default
-    misa = 0x8000000000141101, -- default
-    mscratch = 0x0, -- default
-    mstatus = 0xa00000000, -- default
-    mtval = 0x0, -- default
-    mtvec = 0x0, -- default
-    mvendorid = 0x6361727465736920, -- default
-    pc = 0x1000, -- default
-    satp = 0x0, -- default
-    scause = 0x0, -- default
-    scounteren = 0x0, -- default
-    sepc = 0x0, -- default
-    sscratch = 0x0, -- default
-    stval = 0x0, -- default
-    stvec = 0x0, -- default
+    tx_buffer = {
+      backing_store = {
+        create = false, -- default
+        data_filename = "", -- default
+        dht_filename = "", -- default
+        dpt_filename = "", -- default
+        shared = false, -- default
+        truncate = false, -- default
+      },
+    },
   },
-  ram = {
-    length = 0x4000000,
-    image_filename = "/opt/cartesi/share/images/linux.bin",
-  },
-  rom = {
-    image_filename = "/opt/cartesi/share/images/rom.bin",
-    bootargs = "console=hvc0 rootfstype=ext2 root=/dev/mtdblock0 rw quiet swiotlb=noforce mtdparts=flash.0:-(root)",
-  },
-  htif = {
-    tohost = 0x0, -- default
-    fromhost = 0x0, -- default
-    console_getchar = false, -- default
-    yield_automatic = false, -- default
-    yield_manual = false, -- default
-  },
-  clint = {
-    mtimecmp = 0x0, -- default
+  dtb = {
+    backing_store = {
+      create = false, -- default
+      data_filename = "", -- default
+      dht_filename = "", -- default
+      dpt_filename = "", -- default
+      shared = false, -- default
+      truncate = false, -- default
+    },
+    bootargs = "quiet earlycon=sbi console=hvc0 uio_pdrv_genirq.of_id=generic-uio root=/dev/pmem0 rw init=/usr/sbin/cartesi-init", -- default
+    entrypoint = "", -- default
+    init = "echo \"\
+         .\
+        / \\\\\
+      /    \\\\\
+\\\\---/---\\\\  /----\\\\\
+ \\\\       X       \\\\\
+  \\\\----/  \\\\---/---\\\\\
+       \\\\    / CARTESI\
+        \\\\ /   MACHINE\
+         '\
+\"\
+",
   },
   flash_drive = {
     {
-      start = 0x8000000000000000,
-      length = 0x5000000,
-      image_filename = "/opt/cartesi/share/images/rootfs.ext2",
-      shared = false, -- default
+      backing_store = {
+        create = false,
+        data_filename = "/usr/share/cartesi-machine/images/rootfs.ext2",
+        dht_filename = "",
+        dpt_filename = "",
+        shared = false,
+        truncate = false,
+      },
+      label = "root",
+      length = 0x15dbe000,
+      read_only = false,
+      start = 0x80000000000000,
     },
   },
+  hash_tree = {
+    create = false, -- default
+    hash_function = "keccak256", -- default
+    phtc_filename = "", -- default
+    phtc_size = 0x1000, -- default
+    shared = false, -- default
+    sht_filename = "", -- default
+  },
+  nvram = {},
+  pmas = {
+    backing_store = {
+      create = false, -- default
+      data_filename = "", -- default
+      dht_filename = "", -- default
+      dpt_filename = "", -- default
+      shared = false, -- default
+      truncate = false, -- default
+    },
+  },
+  processor = {
+    backing_store = {
+      create = false, -- default
+      data_filename = "", -- default
+      dht_filename = "", -- default
+      dpt_filename = "", -- default
+      shared = false, -- default
+      truncate = false, -- default
+    },
+    registers = {
+      clint = {
+        mtimecmp = 0x0, -- default
+      },
+      f0 = 0x0, -- default
+      f1 = 0x0, -- default
+      f10 = 0x0, -- default
+      f11 = 0x0, -- default
+      f12 = 0x0, -- default
+      f13 = 0x0, -- default
+      f14 = 0x0, -- default
+      f15 = 0x0, -- default
+      f16 = 0x0, -- default
+      f17 = 0x0, -- default
+      f18 = 0x0, -- default
+      f19 = 0x0, -- default
+      f2 = 0x0, -- default
+      f20 = 0x0, -- default
+      f21 = 0x0, -- default
+      f22 = 0x0, -- default
+      f23 = 0x0, -- default
+      f24 = 0x0, -- default
+      f25 = 0x0, -- default
+      f26 = 0x0, -- default
+      f27 = 0x0, -- default
+      f28 = 0x0, -- default
+      f29 = 0x0, -- default
+      f3 = 0x0, -- default
+      f30 = 0x0, -- default
+      f31 = 0x0, -- default
+      f4 = 0x0, -- default
+      f5 = 0x0, -- default
+      f6 = 0x0, -- default
+      f7 = 0x0, -- default
+      f8 = 0x0, -- default
+      f9 = 0x0, -- default
+      fcsr = 0x0, -- default
+      htif = {
+        fromhost = 0x0, -- default
+        iconsole = 0x2, -- default
+        ihalt = 0x1, -- default
+        iyield = 0x3, -- default
+        tohost = 0x0, -- default
+      },
+      icycleinstret = 0x0, -- default
+      iflags = {
+        H = 0x0, -- default
+        X = 0x0, -- default
+        Y = 0x0, -- default
+      },
+      ilrsc = 0xffffffffffffffff, -- default
+      iprv = 0x3, -- default
+      iunrep = 0x0, -- default
+      marchid = 0x14, -- default
+      mcause = 0x0, -- default
+      mcounteren = 0x0, -- default
+      mcycle = 0x0, -- default
+      medeleg = 0x0, -- default
+      menvcfg = 0x0, -- default
+      mepc = 0x0, -- default
+      mideleg = 0x0, -- default
+      mie = 0x0, -- default
+      mimpid = 0x14, -- default
+      mip = 0x0, -- default
+      misa = 0x800000000014112d, -- default
+      mscratch = 0x0, -- default
+      mstatus = 0xa00000000, -- default
+      mtval = 0x0, -- default
+      mtvec = 0x0, -- default
+      mvendorid = 0x6361727465736920, -- default
+      pc = 0x80000000, -- default
+      plic = {
+        girqpend = 0x0, -- default
+        girqsrvd = 0x0, -- default
+      },
+      satp = 0x0, -- default
+      scause = 0x0, -- default
+      scounteren = 0x0, -- default
+      senvcfg = 0x0, -- default
+      sepc = 0x0, -- default
+      sscratch = 0x0, -- default
+      stval = 0x0, -- default
+      stvec = 0x0, -- default
+      x0 = 0x0, -- default
+      x1 = 0x0, -- default
+      x10 = 0x0, -- default
+      x11 = 0x7ff00000, -- default
+      x12 = 0x0, -- default
+      x13 = 0x0, -- default
+      x14 = 0x0, -- default
+      x15 = 0x0, -- default
+      x16 = 0x0, -- default
+      x17 = 0x0, -- default
+      x18 = 0x0, -- default
+      x19 = 0x0, -- default
+      x2 = 0x0, -- default
+      x20 = 0x0, -- default
+      x21 = 0x0, -- default
+      x22 = 0x0, -- default
+      x23 = 0x0, -- default
+      x24 = 0x0, -- default
+      x25 = 0x0, -- default
+      x26 = 0x0, -- default
+      x27 = 0x0, -- default
+      x28 = 0x0, -- default
+      x29 = 0x0, -- default
+      x3 = 0x0, -- default
+      x30 = 0x0, -- default
+      x31 = 0x0, -- default
+      x4 = 0x0, -- default
+      x5 = 0x0, -- default
+      x6 = 0x0, -- default
+      x7 = 0x0, -- default
+      x8 = 0x0, -- default
+      x9 = 0x0, -- default
+    },
+  },
+  ram = {
+    backing_store = {
+      create = false, -- default
+      data_filename = "/usr/share/cartesi-machine/images/linux.bin",
+      dht_filename = "", -- default
+      dpt_filename = "", -- default
+      shared = false, -- default
+      truncate = false, -- default
+    },
+    length = 0x8000000,
+  },
+  uarch = {
+    processor = {
+      backing_store = {
+        create = false, -- default
+        data_filename = "", -- default
+        dht_filename = "", -- default
+        dpt_filename = "", -- default
+        shared = false, -- default
+        truncate = false, -- default
+      },
+      registers = {
+        cycle = 0x0, -- default
+        halt_flag = 0x0, -- default
+        pc = 0x600000, -- default
+        x0 = 0x0, -- default
+        x1 = 0x0, -- default
+        x10 = 0x0, -- default
+        x11 = 0x0, -- default
+        x12 = 0x0, -- default
+        x13 = 0x0, -- default
+        x14 = 0x0, -- default
+        x15 = 0x0, -- default
+        x16 = 0x0, -- default
+        x17 = 0x0, -- default
+        x18 = 0x0, -- default
+        x19 = 0x0, -- default
+        x2 = 0x0, -- default
+        x20 = 0x0, -- default
+        x21 = 0x0, -- default
+        x22 = 0x0, -- default
+        x23 = 0x0, -- default
+        x24 = 0x0, -- default
+        x25 = 0x0, -- default
+        x26 = 0x0, -- default
+        x27 = 0x0, -- default
+        x28 = 0x0, -- default
+        x29 = 0x0, -- default
+        x3 = 0x0, -- default
+        x30 = 0x0, -- default
+        x31 = 0x0, -- default
+        x4 = 0x0, -- default
+        x5 = 0x0, -- default
+        x6 = 0x0, -- default
+        x7 = 0x0, -- default
+        x8 = 0x0, -- default
+        x9 = 0x0, -- default
+      },
+    },
+    ram = {
+      backing_store = {
+        create = false, -- default
+        data_filename = "", -- default
+        dht_filename = "", -- default
+        dpt_filename = "", -- default
+        shared = false, -- default
+        truncate = false, -- default
+      },
+    },
+  },
+  virtio = {},
 }
-
--- doc/recipes/find-htif-putchar.lua
+++ doc/recipes/find-htif-putchar.lua
@@ -3,12 +3,12 @@ local cartesi = require"cartesi"
 local config = require"config.nothing-to-do"
 local machine = cartesi.machine(config)
 
-local mcycle = machine:read_mcycle()
-local tohost = machine:read_htif_tohost()
+local mcycle = machine:read_reg("mcycle")
+local tohost = machine:read_reg("htif_tohost")
 local line = 0
-while not machine:read_iflags_H() and not machine:read_iflags_Y() do
+while machine:read_reg("iflags_H") == 0 and machine:read_reg("iflags_Y") == 0 do
     machine:run(mcycle+1)
-    local newtohost = machine:read_htif_tohost()
+    local newtohost = machine:read_reg("htif_tohost")
     if tohost ~= newtohost then
         tohost = newtohost
         if tohost & 0xff == 0x0a then
@@ -19,7 +19,7 @@ while not machine:read_iflags_H() and not machine:read_iflags_Y() do
             end
         end
     end
-    mcycle = machine:read_mcycle()
+    mcycle = machine:read_reg("mcycle")
     if mcycle % 10^5  == 0 then
         collectgarbage("collect")
     end
